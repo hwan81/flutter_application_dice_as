7037e4e140abd6e6e5599f3a5b79b0616b2a5711
leejunghwan <hwan@sungil-i.kr>
2023-05-02 17:53:19 +0900

    수정

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,53 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Dice dice = Dice(size: 100);
+  Dice dice = Dice(size: 45);
   late Timer timer;
-  int resultNum = 0;
+  dynamic resultNum = 0;
+  String resultView = '';
+  bool isStart = false;
 
   void start() {
-    timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
-      dice.shake();
-      print(dice.dice[0]);
-      setState(() {
-        resultNum = dice.dice[0];
+    if (!isStart & dice.dice.isNotEmpty) {
+      timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+        dice.shake();
+        setState(() {
+          resultNum = dice.dice[0];
+          isStart = true;
+        });
       });
+    }
+  }
+
+  void pickUp() {
+    if (dice.dice.isNotEmpty && isStart) {
+      setState(() {
+        //  resultView = resultView + ' ' + dice.pick().toString();
+        resultView = '$resultView ${dice.pick()}';
+      });
+      if (dice.dice.isEmpty) {
+        //클래스이름.안에변수이름
+        timer.cancel();
+        setState(() {
+          isStart = false;
+          resultNum = '끝!!';
+        });
+      }
+    }
+  }
+
+  //초기화
+  //결과를 지우기
+  //배열을 다시 초기화=> 원래 크기로 만들기
+  void reset() {
+    setState(() {
+      resultNum = '';
+      resultView = '';
+      dice.init();
+      if (isStart) {
+        timer.cancel();
+      }
+      isStart = false;
     });
   }
 
@@ -43,12 +79,12 @@ class _MyAppState extends State<MyApp> {
                     style: const TextStyle(fontSize: 60),
                   ),
                 )),
-            const Flexible(
+            Flexible(
                 flex: 1,
                 child: Center(
                   child: Text(
-                    '결과',
-                    style: TextStyle(fontSize: 20),
+                    resultView,
+                    style: const TextStyle(fontSize: 20),
                   ),
                 )),
             Flexible(
@@ -64,8 +100,13 @@ class _MyAppState extends State<MyApp> {
                         )),
                     IconButton(
                         iconSize: 100,
-                        onPressed: () {},
+                        onPressed: pickUp,
                         icon: const Icon(Icons.check_circle_outline)),
+                    IconButton(
+                        iconSize: 100,
+                        onPressed: reset,
+                        icon:
+                            const Icon(Icons.settings_backup_restore_outlined)),
                   ],
                 )),
           ],
